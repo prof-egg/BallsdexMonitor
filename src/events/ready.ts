f38ec73b4219c7c58dc6e7a14bd4dff900decedb
Cryptic42069 <69015627+Cryptic42069@users.noreply.github.com>
2024-06-14 02:30:18 -0400

    fill message cache on startup

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -4,12 +4,23 @@ import Debug from "../lib/util/Debug.js"
 import messageConfig from "../config/messages.json" assert { type: "json" }
 import clientconfig from "../config/client.json" assert { type: "json" }
 import FHH from "../lib/handlers/HandlerHub.js"
+import { SpawnManager } from "../lib/handlers/countryballs/SpawnHandler.js"
 
 const eventType = Events.ClientReady
 
 const eventFunction: IEventFunc<typeof eventType> = async (client, loggerID, readyClient) => {
+
+    // Do command handling
     FHH.CommandHandler.cacheData(readyClient)
     await FHH.CommandHandler.loadSlashCommandFolder("dist/commands")
+
+    // Fill spawncooldown message cache for guild
+    let homeGuild = await client.guilds.fetch(clientconfig.homeGuild.id)
+    SpawnManager.ensureGuildCooldown(homeGuild)
+    let cooldown = SpawnManager.getGuildSpawnCooldown(clientconfig.homeGuild.id)
+    await cooldown?.resetMessageCache(homeGuild)
+    
+    // Set presence
     readyClient.user.setActivity(messageConfig.clientPresence);
     Debug.logImportant(`${clientconfig.name} is online!`, loggerID)
 }

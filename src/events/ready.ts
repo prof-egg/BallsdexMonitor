3aea3de00739e597dfdebf64bb155fbdd40934e0
Cryptic42069 <69015627+Cryptic42069@users.noreply.github.com>
2024-06-25 16:15:35 -0400

    load rarities in ready event

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -5,6 +5,7 @@ import messageConfig from "../config/messages.json" assert { type: "json" }
 import clientconfig from "../config/client.json" assert { type: "json" }
 import FHH from "../lib/handlers/HandlerHub.js"
 import { SpawnManager } from "../lib/handlers/countryballs/SpawnHandler.js"
+import RarityScraper from "../lib/handlers/countryballs/RarityScraper.js"
 
 const eventType = Events.ClientReady
 
@@ -26,6 +27,9 @@ const eventFunction: IEventFunc<typeof eventType> = async (client, loggerID, rea
         Debug.logWarning("Unable to activate monitor", "ReadyEvent")
     }
     
+    // Load rarities for rarity raw wiki
+    await RarityScraper.loadRarities()
+
     // Set presence
     readyClient.user.setActivity(messageConfig.clientPresence);
     Debug.logImportant(`${clientconfig.name} is online!`, loggerID)
